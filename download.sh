cd0bd55c27415e16a40f94a355d0145dbb99181a
b09401064 <b09401064@g.ntu.edu.tw>
2024-12-25 10:50:34 -0500

    Update download bash file

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 # To get all models
 wget -O SDXL.zip https://www.dropbox.com/scl/fi/wr8fpypgp8nuhwvhl6jo3/SDXL.zip?rlkey=82wfvgjl5vrx5jtakvzvfrbsf&dl=1 
 unzip SDXL.zip
-mv SDXL experiments/pretrained_models/
+mv SDXL experiments/
 rm SDXL.zip
 rm 'SDXL.zip?rlkey=82wfvgjl5vrx5jtakvzvfrbsf'
+
+# To get CC models
+
