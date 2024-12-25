da9db0ab0178cc2458512932fb284f5fda7f0b04
b09401064 <b09401064@g.ntu.edu.tw>
2024-12-25 10:45:28 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-To get all models
+# To get all models
 wget -O SDXL.zip https://www.dropbox.com/scl/fi/wr8fpypgp8nuhwvhl6jo3/SDXL.zip?rlkey=82wfvgjl5vrx5jtakvzvfrbsf&dl=1 
 unzip SDXL.zip
 mv SDXL experiments/pretrained_models/
