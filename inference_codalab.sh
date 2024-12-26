72a7cd29fbe7aa24bda4bab530211d37b5daf150
b09401064 <b09401064@g.ntu.edu.tw>
2024-12-26 07:56:10 -0500

    Update README and bash scripts

--- inference_codalab.sh
+++ inference_codalab.sh
@@ -3,15 +3,15 @@
 # $1 : "<Path to output image folder>"
 
 if [ -z "$1" ]; then
-  outimg_dir="./test_output_peer"
+  outimg_dir="./test_output_codalab"
 else
   outimg_dir="$1"
 fi
 
 # Folder contains prompt 0's config files
-prompt1_folder="./configs/inference/prompt0"
+prompt0_folder="./configs/inference/prompt0"
 
-for config_file in "$prompt1_folder"/*; do
+for config_file in "$prompt0_folder"/*; do
   echo "$config_file" >> inference_config_check
   python3 sample.py --config_file "$config_file" --outimg_dir "$outimg_dir" --prompt_num "0"
 done
